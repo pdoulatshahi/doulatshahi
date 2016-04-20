3d640ff658f2a25cc5c7774ffe8950317d0a25cf
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2016-04-20 11:09:56 -0400

    Final changes from Tuesday

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 	$(".nav-links a").click(function(e){
 		e.preventDefault();
+		var index = $(this).parent("li").index();
+		console.log(index);
+		var el = $("section:eq(" + index + ")");
+		console.log(el)
+		$('html,body').animate({
+		   scrollTop: el.offset().top - 30
+		}, 1000);
+
 	})
 })
